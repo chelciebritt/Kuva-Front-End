db1b439065eb0d5897d1c77573d7613cc427ab36
James Mendenhall <Nattysoccer9@yahoo.com>
2017-09-18 18:56:00 -0600

    styling

--- js/app.js
+++ js/app.js
@@ -1,8 +1,28 @@
+$(document).ready(function() {
+  $('.materialboxed').materialbox();
+});
+
 $('.datepicker').pickadate({
   selectMonths: true, // Creates a dropdown to control month
-  selectYears: 15, // Creates a dropdown of 15 years to control year,
+  selectYears: 2, // Creates a dropdown of 15 years to control year,
   today: 'Today',
   clear: 'Clear',
   close: 'Ok',
   closeOnSelect: false // Close upon selecting a date,
 });
+
+// Adding email fields dynamically
+
+var counter = 1;
+var limit = 15;
+
+function addInput(divName) {
+  if (counter == limit) {
+    alert("You have reached the limit of adding " + counter + " inputs");
+  } else {
+    var newdiv = document.createElement('div');
+    newdiv.innerHTML = "Enter email " + (counter + 1) + " <br><input type='text' name='myInputs[]'>";
+    document.getElementById(divName).appendChild(newdiv);
+    counter++;
+  }
+}
