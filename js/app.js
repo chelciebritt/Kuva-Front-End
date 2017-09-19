2c0d0381c0a0490da64fd99ddd5388fc7be79b69
Miriam Quintana <quintana.miriam.cc@gmail.com>
2017-09-19 14:37:57 -0600

    app.js changes

--- js/app.js
+++ js/app.js
@@ -2,6 +2,35 @@ $(document).ready(function() {
   $('.materialboxed').materialbox();
 });
 
+
+$(function() {
+  var API = 'https://api.instagram.com/v1/users/self/media/recent/?access_token='
+  var API_KEY = '634657769.f866f3d.214922cf24f84b52bdcb85eee0ca8be1';
+
+  $.getJSON(API + API_KEY + '&callback=?', function(insta) {
+    $.each(insta.data, function(photos, src) {
+      if (photos === 20) {
+        return false;
+}
+        var imgUrl = src.images.standard_resolution.url
+
+        $("#picDiv").append(`
+                          <div class="col s12 m6 l4 ">
+                            <div class="card">
+                              <div class="card-image">
+                                <img class="materialboxed" src=${imgUrl}>
+                              </div>
+                            </div>
+                          </div>
+                        `)
+                      });
+                    });
+                  });
+
+
+
+
+
 $('.datepicker').pickadate({
   selectMonths: true, // Creates a dropdown to control month
   selectYears: 2, // Creates a dropdown of 15 years to control year,
